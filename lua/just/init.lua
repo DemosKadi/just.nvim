6cf6c60b4140f8f323e490d38f0fb4c4773a7897
Tom Dannemann <tom.dannemann@mailbox.org>
2024-12-19 16:04:00 +0100

    made runner and window position configurable

--- lua/just/init.lua
+++ lua/just/init.lua
@@ -1,6 +1,38 @@
-local M = {}
+local M = {
+    config = {},
+}
 
-M.setup = function()
+---@class defaults
+local defaults = {
+    -- Position of the output window
+    -- 'aboce', 'right', 'below', 'left'
+    ---@type string
+    window = 'right',
+    -- Function that executes the actual just command
+    -- By default it creates a terminal window, with 1/3 of the height or width of the current window, and prints the
+    -- output of the just recipe in there
+    ---@param just_args [string]
+    runner = function(just_args)
+        local current_window_handle = vim.api.nvim_get_current_win()
+        local current_window_config = vim.api.nvim_win_get_config(current_window_handle)
+        print(vim.inspect(current_window_config))
+
+        local buf_handle = vim.api.nvim_create_buf(true, false)
+        vim.api.nvim_open_win(buf_handle, true, {
+            win = current_window_handle,
+            width = math.floor(current_window_config.width / 5 * 2),
+            height = math.floor(current_window_config.height / 5 * 2),
+            split = M.config.window,
+        })
+        vim.fn.termopen(vim.list_extend({ 'just' }, just_args))
+    end,
+}
+
+---@param opts defaults
+M.setup = function(opts)
+    M.config = opts or {}
+    M.config = vim.tbl_extend('keep', M.config, defaults)
+
     vim.api.nvim_create_user_command('Just', function(input)
         M.run(input.fargs)
     end, {
@@ -10,6 +42,7 @@ M.setup = function()
     })
 end
 
+---@return any
 M.dump = function()
     if not M.can_just_run() then
         vim.notify('No justfile found', vim.log.levels.ERROR)
@@ -20,6 +53,7 @@ M.dump = function()
     return vim.fn.json_decode(dump.stdout)
 end
 
+---@return [string]
 M.recipes = function()
     if not M.can_just_run() then
         vim.notify('No justfile found', vim.log.levels.ERROR)
@@ -35,19 +69,17 @@ M.recipes = function()
     return recipes
 end
 
+---@param just_args [string]
 M.run = function(just_args)
     if not M.can_just_run() then
         vim.notify('No justfile found', vim.log.levels.ERROR)
         return
     end
 
-    local args = vim.list_extend({ 'just' }, just_args)
-    if vim.api.nvim_get_option_value('modified', { buf = vim.api.nvim_get_current_buf() }) then
-        vim.api.nvim_command([[new]])
-    end
-    vim.fn.termopen(args)
+    M.config.runner(just_args)
 end
 
+---@return boolean
 M.can_just_run = function()
     -- try a dry-run and check the return code
     local result = vim.system({ 'just', '--dry-run' }):wait()
